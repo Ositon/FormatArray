7b37f9d31fa8b4ab0ae998d0a1aa34554ff8a5b1
wrigzilla <matt.wrigley@ntlworld.com>
2015-01-14 16:25:44 +0000

    Fixed  , append on last go
    Have fixed the loop so that on the last iteration it does not append
    another comma.

--- format.cpp
+++ format.cpp
@@ -5,23 +5,36 @@ using namespace std ;
 
 int main()
 {
-  string line ; int i ; string newLine ;
-
+  string line ; int i ; string newLine = "[ " ; int max ;
   ifstream read( "list.txt" ) ; // create input filestream object to read txt file.
   if ( ! read )	// throw exception if unable to read file
   {
     cout << "Error Opening Input File" << endl ;	// error message
 	return -1 ;
   }
-  for ( i = 0 ; ! read.eof() ; i++ ) // .eof() function check to see if end of file.  Does loop while it is not the end of file.
+  for ( i = 0 ; ! read.eof() ; i++)
   {
     getline( read , line ) ;
-	newLine += line ;  // appends lines from getline to newLine string
   }
+  max = i ; // max = total numebr of lines on txt file
+  cout << max << endl ;
+  read.close() ; read.open( "list.txt" ) ;  // closes filestream and then reopens same file
+  for ( i = 0 ; ! read.eof() ; i++ ) // .eof() function check to see if end of file.  Does loop while it is not the end of file.
+  {
+    if ( i < ( max - 1 ) ) // if not last line append ' ,  at end, if last line append ' instead.  Max -1 to account for i starting at 0 
+	{
+	  getline( read , line ) ;
+	  newLine += "'" + line + "' , " ;  // appends lines from getline to newLine string
+	}
+	else
+	{
+	  getline( read , line ) ;
+	  newLine += "'" + line + "'" ;
+	}
+  }
+  newLine += " ]" ;
   cout << newLine << endl ;
-  
   read.close() ; //close filestream
   cout << "Iterations: " << i << endl ;
-  
   return 0 ;
 }

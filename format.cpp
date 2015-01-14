19744f5d513d46232b438012c95a33e2b9d2176f
wrigzilla <matt.wrigley@ntlworld.com>
2015-01-14 15:43:17 +0000

    Reads file and appends into one string
    Reads file and appends into one string. Code compiles and works.

--- format.cpp
+++ format.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std ;
 
 int main()
 {
+  string line ; int i ; string newLine ;
+
+  ifstream read( "list.txt" ) ; // create input filestream object to read txt file.
+  if ( ! read )	// throw exception if unable to read file
+  {
+    cout << "Error Opening Input File" << endl ;	// error message
+	return -1 ;
+  }
+  for ( i = 0 ; ! read.eof() ; i++ ) // .eof() function check to see if end of file.  Does loop while it is not the end of file.
+  {
+    getline( read , line ) ;
+	newLine += line ;  // appends lines from getline to newLine string
+  }
+  cout << newLine << endl ;
   
-  
+  read.close() ; //close filestream
+  cout << "Iterations: " << i << endl ;
   
   return 0 ;
 }
